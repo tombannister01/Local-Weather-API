b271a210160a3ffbfe679439b107064d1e6dd5a7
Emma Hobday <emma.hobday@gmail.com>
2020-03-12 13:24:30 +0000

    clock working

--- project-2-starter-code/src/components/SingleCity.js
+++ project-2-starter-code/src/components/SingleCity.js
@@ -7,26 +7,59 @@ class SingleCity extends React.Component {
   constructor() {
     super()
     this.state = {
-      forecast: {}
+      intervalIdBoolean: true,
+      intervalID: setInterval(() => {
+        console.log(new Date(Date.now()))
+      }, 1000),
+      forecast: {
+        currently: {},
+        minutely: {},
+        hourly: {
+          data: []
+        },
+        daily: {}
+      }
     }
   }
 
 
   componentDidMount() {
-    console.log(this.props)
+    // clearInterval(this.state.intervalID)
     const data = this.props.location.state
-    console.log(data.city.latlong)
-    axios.get(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/b5ed109d2c55c56135ed4fc656a0f25c/${data.city.latlong}`, )
+    // console.log(data.city.latlong)
+    axios.get(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/b5ed109d2c55c56135ed4fc656a0f25c/${data.city.latlong}`)
       .then(res => this.setState({ forecast: res.data }))
       .catch(err => console.error(err))
+
+
+  
+
+
 
   }
 
 
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalID)
+  }
+
+
+
+
   render() {
     const forecast = this.state.forecast
-    console.log(forecast)
-    if (!forecast) return null
+    // console.log(forecast)
+    let counter = 0
+
+
+
+
+
+
+    // const  { summary, icon, temperature, apparentTemperature, precipProbability } = forecast.hourly.data
+    if (!forecast) return <h1>hello</h1>
+    // console.log(
 
     return <div className="tile is-ancestor">
       <div className="tile is-vertical is-8">
@@ -34,28 +67,54 @@ class SingleCity extends React.Component {
           <div className="tile is-parent is-vertical">
             <article className="tile is-child box">
               {this.props.location.state.city.name}
+              <img src={this.props.location.state.city.image} />
             </article>
             <article className="tile is-child box">
               {forecast.timezone}
             </article>
             <article className="tile is-child box">
-              Hello  hourly
+              Current Weather
+              {forecast.currently.summary}
+              <img src={`https://icons8.com/icon/658/${forecast.currently.icon}`} />
+              Temperature: {Math.round(forecast.currently.temperature)}°C
+              Feels like {Math.round(forecast.currently.apparentTemperature)}°C
+              Chance of rain: {forecast.currently.precipProbability}%
+
             </article>
             <article className="tile is-child box">
-              Hello  daily
+              In the next hour
+              {forecast.minutely.summary}
+              {forecast.minutely.icon}
+            </article>
+            <article className="tile is-child box">
+              Rest of the day
+              {forecast.hourly.summary}
+
+
             </article>
           </div>
-          <div className="tile is-parent">
-            <article className="tile is-child box">
-              <img src={this.props.location.state.city.image}/>
+          <div className="tile is-parent vertical">
+            <article className="tile is-child box hourly">
+              The next 24 hours:
+              {forecast.daily.summary}
+              {forecast.hourly.data.map((hourly, key) => {
+                return <p key={key}>Hour {counter += 1}:00 - {hourly.summary} - {Math.round(hourly.temperature)}°C</p>
+              })}
+
+            </article>
+            <article className="tile is-child box hourly">
+              Rest of the day
+              {forecast.hourly.summary}
+
+
             </article>
           </div>
         </div>
-        <div className="tile is-parent">
+        {/* <div className="tile is-parent">
           <article className="tile is-child box">
-            Hello 7 day cast
+
           </article>
-        </div>
+        </div> */}
       </div>
       <div className="tile is-parent">
         <article className="tile is-child box">
