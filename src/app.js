d8ff896be552af0c4bd31778579add00868d3468
Emma Hobday <emma.hobday@gmail.com>
2020-04-24 14:24:02 +0100

    updated

--- src/app.js
+++ src/app.js
@@ -8,15 +8,15 @@ import Home from './components/Home'
 import Cities from './components/Cities'
 import SingleCity from './components/SingleCity'
 
-const App = () => (
-  <BrowserRouter>
+const App = () => {
+  return <BrowserRouter basename="/project-2">
     <Switch>
       <Route exact path="/cities/:city" component={SingleCity} />
       <Route exact path="/cities" component={Cities} />
       <Route exact path="/" component={Home} />
     </Switch>
   </BrowserRouter>
-)
+}
 
 ReactDOM.render(
   <App />,
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve('dist'),
-    publicPath: '/'
+    publicPath: '/project-2'
   },
   module: {
     rules: [
